d42e64ebe0c17a2d7d3cc018103d17b6d9540f1f
erlaangga <erlangga.ip@gmail.com>
2017-12-11 11:20:47 +0700

    countdown dan kompresi gambar

--- index.js
+++ index.js
@@ -1,34 +1,78 @@
-setTimeout("jam()", 1000);
-var target_jam = 9;
+// setTimeout("jam()", 1000);
+setInterval("jam()", 1000);
+var target_jam = 8;
+var target_menit = 59;
+var target_detik = 60;
+var target_tanggal = 13;
+var target_bulan = 0;
+var target_tahun = 2018;
+var tanggal;
+var bulan;
+var hari;
+var tahun;
+var sisa_hari = 0;
+var sisa_jam = 23;
+var sisa_menit = 59;
+var sisa_detik = 59;
+
 function jam(){
-	a = document.getElementById('jam')
+	el_jam = document.getElementById('jam');
+	menit = document.getElementById('menit');
+	detik = document.getElementById('detik');
+	
 	var date = new Date();
-	var tanggal = date.getDate();
-	var bulan = date.getMonth();
-	var hari = date.getDay();
+	tanggal = date.getDate();
+	bulan = date.getMonth();
+	hari = date.getDay();
+	tahun = date.getFullYear();
+	sisa_hari = 0;
+	sisa_jam = 23;
+	sisa_menit = 59;
+	sisa_detik = 59;
+
 	var namabulan = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September','Oktober','November','Desember'];
 	var jam = date.getHours();
-	if (jam < 9) {
-		jam = target_jam - jam;
+	if(bulan == 11){
+		sisa_hari = 31 - tanggal + target_tanggal;
+	}
+	else if (bulan == 0) {
+		sisa_hari = target_tanggal - hari;
+	}
+	if (jam <= target_jam) {
+		sisa_jam = target_jam - jam;
 	}
 	else {
-		
+		sisa_jam = 24 - jam + target_jam;
+		sisa_hari--;
 	}
-	var menit = 59 - date.getMinutes();
-	var detik = 59 - date.getSeconds();
-	a.innerHTML = cekwaktu(jam)+':'+cekwaktu(menit)+':'+cekwaktu(detik);
+	sisa_menit =  target_menit - date.getMinutes();
+	/*if(sisa_menit == 60){
+		sisa_menit = 0;
+	}*/
+	sisa_detik = target_detik - date.getSeconds();
+	if(sisa_detik == 60){
+		sisa_detik = 0;
+	}
+	el_jam.innerHTML = cekwaktu(sisa_jam) + "Jam";
+	menit.innerHTML = cekwaktu(sisa_menit) + "Menit";
+	detik.innerHTML = cekwaktu(sisa_detik) + "Detik";
+
 	if (tanggal > 13 && bulan == 11 && tahun == 117) {
 		tanggal = 31 - 13;
 	}
 	else if(tanggal == 13 && bulan == 0 && tahun == 118){
 		bulan = tanggal = 0;
 	}
-	document.getElementById('tanggal').innerHTML = cekwaktu(tanggal);
-	setTimeout("jam()", 1000);
+
+	if(sisa_hari){
+		document.getElementById('hari').innerHTML = cekwaktu(sisa_hari) + " Hari";
+	}
+	// karena setTimeout hanya menjalankan kode sekali
+	// setTimeout("jam()", 1000);
 }
 
 function cekwaktu(nominal){
-	return (nominal>9&&nominal>0?nominal:'0'+nominal)
+	return (nominal>9?nominal:'0'+nominal)
 }
 
 function tes(){
