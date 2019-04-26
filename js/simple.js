e9ae03d8a1ba90ce64fa3689ea4a0b0d91112435
Erlan <erlangga.ip@gmail.com>
2019-04-26 14:17:08 +0700

    add odoo2 and imp css, js

--- js/simple.js
+++ js/simple.js
@@ -1,7 +1,7 @@
+"use strict";
 document.onreadystatechange = function(e1){
 			var togg_menu = document.getElementById("toggler-menu");
 			togg_menu.onclick = function(e){
-				console.log(menu);
 				if (!menu.classList.contains("active")) {
 					menu.classList.add("active");
 					menu.style.paddingBottom = '5px';
